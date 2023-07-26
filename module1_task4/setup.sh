51a0c29e1d5dd8e3afa4f342f5da35c84eb4c52c
Enyel019 <el_dominic01@hotmail.com>
2023-07-25 21:22:02 -0400

    new update

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -3,6 +3,6 @@
 apt-get update
 apt-get install -y wget
 apt-get install -y make
-wget https://github.com/gohugoio/hugo/releases/download/v0.115.4/hugo_0.115.4_Linux-64bit.deb
-dpkg -i hugo_0.115.4_Linux-64bit.deb
+wget https://github.com/gohugoio/hugo/releases/download/v0.85.0/hugo_0.85.0_Linux-64bit.deb
+dpkg -i hugo_0.85.0_Linux-64bit.deb
 make build
