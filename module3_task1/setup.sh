b8c1253ef602142ddc1ac21c90bb93bb7d6a7deb
Enyel019 <el_dominic01@hotmail.com>
2023-08-14 22:34:16 -0400

    new update

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# Install git if it's not installed
-if ! command -v git &> /dev/null; then
+# Check if wget is installed
+if ! command -v wget &> /dev/null; then
     sudo apt-get update
-    sudo apt-get install -y git
+    sudo apt-get install -y wget
 fi
 
-# Install make if it's not installed
-if ! command -v make &> /dev/null; then
-    sudo apt-get update
-    sudo apt-get install -y make
-fi
+# Download and install Hugo
+HUGO_VERSION=0.85.0
+HUGO_DOWNLOAD_URL="https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.deb"
+HUGO_DEB="hugo_${HUGO_VERSION}_Linux-64bit.deb"
 
+wget "$HUGO_DOWNLOAD_URL"
+sudo dpkg -i "$HUGO_DEB"
 
+# Clean up downloaded .deb file
+rm "$HUGO_DEB"
