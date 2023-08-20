7565ba329b8868b390536e3179197ff2df81096c
Enyel019 <el_dominic01@hotmail.com>
2023-08-20 19:07:28 -0400

    new setup

--- module3_task5/setup.sh
+++ module3_task5/setup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+sudo apt update
+sudo apt upgrade
+sudo apt install -y wget make shellcheck zip
 
-apt-get update
-apt-get install -y wget
-apt-get install -y make
-wget https://github.com/gohugoio/hugo/releases/download/v0.85.0/hugo_0.85.0_Linux-64bit.deb
-dpkg -i hugo_0.85.0_Linux-64bit.deb
-make build
+# Download Go
+wget https://go.dev/dl/go1.20.1.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.20.1.linux-amd64.tar.gz
+rm ./go1.20.1.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+
+# Download the Hugo executable
+wget https://github.com/gohugoio/hugo/releases/download/v0.110.0/hugo_0.110.0_Linux-64bit.tar.gz
+tar -C /usr/local/bin -xzf hugo_0.110.0_Linux-64bit.tar.gz
+rm ./hugo_0.110.0_Linux-64bit.tar.gz
